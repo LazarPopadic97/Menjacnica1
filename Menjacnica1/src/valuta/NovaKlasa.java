13c14c7ad2cd655a2ceca0da0d7e4fa61f1645a2
LazarPopadic97 <LazarPopadic97>
2020-03-02 11:00:28 +0100

    klasa koja implementira interfejs je do kraja implementirana

--- Menjacnica1/src/valuta/NovaKlasa.java
+++ Menjacnica1/src/valuta/NovaKlasa.java
@@ -1,26 +1,29 @@
 package valuta;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import interfejs.SpecifikacijaInterfejs;
 
 public class NovaKlasa implements SpecifikacijaInterfejs{
 
+	List<Valuta> valute=new ArrayList<Valuta>();
 	@Override
 	public void dodajKurs(double kurs, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
 		
+		System.out.println("Nesto");
 	}
 
 	@Override
 	public void obrisiKurs(double kurs, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		System.out.println("Nesto");
 		
 	}
 
 	@Override
 	public double nadjiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
