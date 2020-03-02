25cc145c69177e75851f9515600afc3dfae4ece8
LazarPopadic97 <LazarPopadic97>
2020-03-02 10:55:43 +0100

    napravljena nova klasa NovaKlasa koja implementira interfejs

---
+++ Menjacnica1/src/valuta/NovaKlasa.java
@@ -0,0 +1,27 @@
+package valuta;
+
+import java.util.GregorianCalendar;
+
+import interfejs.SpecifikacijaInterfejs;
+
+public class NovaKlasa implements SpecifikacijaInterfejs{
+
+	@Override
+	public void dodajKurs(double kurs, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKurs(double kurs, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double nadjiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
